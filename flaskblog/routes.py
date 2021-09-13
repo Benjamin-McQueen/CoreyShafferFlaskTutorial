b9a575cc9de6c2d35f9bb775fc3fa411698eb260
Benjamin McQueen <Benjamin.McQueen@x2i.systems>
2021-09-13 16:56:05 -0400

    Proceeded to and completed part 5 of the tutorial series

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,10 @@
 import flask
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from flask import render_template, flash, redirect, url_for
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'bjakrbgvfdfgaesgribgfahksdbl'
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flaskblog import app
+
 
 posts = [
     {
@@ -47,13 +48,6 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash ('you have been logged in!', 'success')
+        flash('you have been logged in!', 'success')
         return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# python flaskblog.py
-# :)
