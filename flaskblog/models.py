ef365ce8715de15ff1b355cebbf2b08e29486e74
Benjamin McQueen <Benjamin.McQueen@x2i.systems>
2021-09-14 16:57:18 -0400

    Proceeded to and completed part 7 of the tutorial series, and also up to 17:28 of part 8.

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
 
     __tablename__ = 'user'
 
@@ -23,9 +29,9 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    date_pasted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_pasted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
